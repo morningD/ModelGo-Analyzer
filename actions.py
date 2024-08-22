7f4c66babe979533f50b7974f22344e8bbd6c773
morningD <duanmoming@gmail.com>
2024-08-22 14:46:52 +0800

    + Basical workflow generation from python input

---
+++ actions.py
@@ -0,0 +1,81 @@
+from collections import defaultdict
+import logging
+from copy import deepcopy
+from rdflib import Graph, Namespace, Literal, BNode
+from rdflib.namespace import RDF, RDFS
+
+""" <actions.py>
+This file contains the functions that can use to create a Turtule file that describe the ML workflow based on users' input,
+i.e., combine(m1, m2 ,finetune(model, data)) -> workflow.ttl (RDF graph representation of this workflow),
+after then we can visuablize this RDF graph and analysis the conflict by reasoner EYE.
+"""
+
+# Set the Namespaces
+MG = Namespace("http://www.modelgo.li/rdf/terms#")
+EX = Namespace("http://example.org/ns#")
+
+# Set the prefix binding list
+prefixs = [
+    ("mg", MG),     # mg:xxx
+    ("", EX)        # :xxx
+]
+
+# The function that create a empty RDF Graph with predefined namespace
+def create_base_graph(prefixs=prefixs):
+    g = Graph()
+    for (prefix, ns) in prefixs:
+        g.bind(prefix, ns)
+    return g
+
+class Work(object):
+    def __init__(self, name: str, type: str, form: str):
+        self.name = name
+        self.type = type # software/data/model or mixed-type (mix of works with different type)
+        self.form = form # code/linguistics/vision/exe/saas or raw-form/binary-form/service-form
+        #self.license_name = license_name # MIT, Apache-2.0, Unlicense
+        
+class Workflow(object):
+    def __init__(self, prefixs=None):
+        if prefixs: self.graph = create_base_graph(prefixs)
+        else: self.graph = create_base_graph()
+
+    def merge(self, flows): # Merge RDF graph
+        if isinstance(flows, Workflow):
+            self.graph += flows.graph
+        if isinstance(flows, list):
+            for f in flows:
+                self.graph += f.graph
+        return
+
+# We maintain a function that generate a incremental number for each action
+def gen_action_num():
+    current: int = 0
+    def next_num() -> int:
+        nonlocal current
+        current += 1
+        return current
+    return next_num
+anum = gen_action_num()
+
+def register_license(target_work: Work, target_license: str='Unlicense', action_label: str='reg') -> Workflow:
+    flow = Workflow() # Create a new workflow contains this register action
+    g = flow.graph
+    wiri = EX[target_work.name]
+    
+    # Add the Work subject in a new RDF graph
+    g.add((wiri, RDF.type, MG.work))
+    g.add((wiri, MG.workName, Literal(target_work.name)))
+    g.add((wiri, MG.hasWorkType, Literal(target_work.type)))
+    g.add((wiri, MG.hasWorkForm, Literal(target_work.form)))
+    
+    # Add the Register action in this graph
+    action_id = action_label + str(anum())
+    airi = EX[action_id]
+    g.add((airi, RDF.type, MG.RegisterLicense))
+    g.add((airi, MG.actionId, Literal(action_id)))
+    bn = BNode() # Blank node of mg:hasInput
+    g.add((airi, MG.hasInput, bn))
+    g.add((bn,MG.targetWork, wiri))
+    g.add((bn,MG.targetLicense, MG[target_license]))
+
+    return flow
